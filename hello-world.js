f78941220065ec98a889aaafcf385e5f870c4509
Andrei Merzliakov <codymaverick@bk.ru>
2022-01-28 20:02:14 +0300

    Edit hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,11 +1,8 @@
-
+//
 // This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-function hello() {
-    console.log('Hello, World!');
+export function hello() {
     return 'Hello, World!';
-}
-
-hello()
+  }  
