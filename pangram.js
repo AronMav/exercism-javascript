7ecaeaf038cab91d15536cc2bdee861cb4d69c05
Andrei Merzliakov <codymaverick@bk.ru>
2022-02-22 19:53:47 +0300

    Create pangram.js

---
+++ pangram.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isPangram = (string) => {
+    for (let letter of ALPHABET) {
+        if (!string.includes(letter) 
+            && !string.includes(letter.toUpperCase())) return false
+    }
+    return true
+};
+  
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
